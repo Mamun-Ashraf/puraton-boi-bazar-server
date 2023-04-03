2a2b20bc4df0903a2d949f090a313bc0d4743cbe
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-04-03 22:17:42 +0600

    added category/:id route

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// user: bookBazar
+// pass: KSMa4aKkygAA1AEx
+
+
+const uri = "mongodb+srv://bookBazar:KSMa4aKkygAA1AEx@cluster0.laf8zrf.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoryCollection = client.db('bookBazar').collection('books');
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const categories = await categoryCollection.find().toArray();
+            res.send(categories);
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const category = await categoryCollection.findOne(query);
+            res.send(category);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+    res.send('Hello from puraton boi bazar');
+});
+
+app.listen(port, () => {
+    console.log(`puraton boi bazar is running on ${port}`);
+})
