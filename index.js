bb69971d1eebcd509d25958ad6031aa78c28d1f0
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-06-23 05:37:57 +0600

    added dashboard api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,9 +32,76 @@ async function run() {
             res.send(category);
         })
 
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN)
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: '' })
+        })
+
+        app.get('/users', async (req, res) => {
+            const filter = {};
+            const users = await usersCollection.find(filter).toArray();
+            res.send(users)
+        })
+
+
+
+        app.get('/users/allbuyers', async (req, res) => {
+            const userType = req.query.userType;
+            const filter = { userType }
+            const allbuyers = await usersCollection.find(filter).toArray();
+            res.send(allbuyers);
+        })
+
+        app.get('/users/allsellers', async (req, res) => {
+            const userType = req.query.userType;
+            const filter = { userType }
+            const allsellers = await usersCollection.find(filter).toArray();
+            res.send(allsellers);
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            };
+            const result = await usersCollection.updateOne(query, updatedDoc, options);
+            res.send(result);
+        })
+        app.put('/users/allsellers/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    VerificationStatus: 'verified'
+                }
+            };
+            const result = await usersCollection.updateOne(query, updatedDoc, options);
+            console.log(result);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(filter);
+            console.log(result);
             res.send(result);
         })
     }
